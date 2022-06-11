44cab2335e34a7ce6dd89a4c0b2e3a22c8e137b4
sairevanth00 <86770889+sairevanth00@users.noreply.github.com>
2022-06-11 19:56:11 +0530

    first commit

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
-import InfoIcon from "@mui/icons-material/Info";
 import FileBase from "react-file-base64";
 import { useDispatch } from "react-redux";
 import useStyles from "./styles";
@@ -9,6 +8,7 @@ import { useSelector } from "react-redux";
 
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
+    creator: "",
     title: "",
     message: "",
     tags: "",
@@ -19,7 +19,6 @@ const Form = ({ currentId, setCurrentId }) => {
   );
 
   const classes = useStyles();
-  const user = JSON.parse(localStorage.getItem("profile"));
   const dispatch = useDispatch();
   useEffect(() => {
     if (post) setPostData(post);
@@ -27,30 +26,17 @@ const Form = ({ currentId, setCurrentId }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (currentId === 0) {
-      dispatch(createPost({ ...postData, name: user?.result?.name }));
+    if (currentId) {
+      dispatch(updatePost(currentId, postData));
     } else {
-      dispatch(
-        updatePost(currentId, { ...postData, name: user?.result?.name }),
-      );
+      dispatch(createPost(postData));
     }
     clear();
   };
-
-  if (!user?.result?.name) {
-    return (
-      <Paper className={classes.paper}>
-        <Typography variant="h6" align="center">
-          <InfoIcon className={classes.root} />
-          Please Sign In to create your own memories and like other's memories
-        </Typography>
-      </Paper>
-    );
-  }
-
   const clear = () => {
     setCurrentId(null);
     setPostData({
+      creator: "",
       title: "",
       message: "",
       tags: "",
@@ -68,6 +54,16 @@ const Form = ({ currentId, setCurrentId }) => {
         <Typography variant="h6">
           {currentId ? "Editing" : "Creating"} a Memory
         </Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
         <TextField
           name="title"
           variant="outlined"
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,6 @@
 import { combineReducers } from "redux";
 import posts from "./posts";
-import auth from "./auth";
 
-const reducers = combineReducers({
+export default combineReducers({
   posts,
-  auth,
 });
-
-export default reducers;
